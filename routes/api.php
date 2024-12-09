33afc3139a8fee188dba87e88ac198d3af4be160
Petar Simonovic <petar.simonovic@datadesign.me>
2024-12-09 20:49:58 +0100

    fix

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\ScoreController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/save_score', [ScoreController::class, 'saveScore']);
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\ScoreController;
 use App\Models\Score;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $scores = Score::all()->sortByDesc('score');
+    $scores = Score::all()->sortByDesc('score')->take(10);
     return view('welcome', ['scores' => $scores]);
 });
+
+Route::post('/save_score', [ScoreController::class, 'saveScore']);
 
 Route::get('/download', function () {
     $path = storage_path('app/public/tetris.zip');
